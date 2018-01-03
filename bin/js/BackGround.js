9452be155bab2ab287ad420ac122d0611bc8bcff
LAPTOP-QE08G43O\admin <xiongwz@ljfkj.com>
2018-01-03 10:14:27 +0800

    添加两个背景，并在定时器里面实现背景的滚动

--- bin/js/BackGround.js
+++ bin/js/BackGround.js
@@ -17,10 +17,26 @@ var BackGround = /** @class */ (function (_super) {
     }
     BackGround.prototype.init = function () {
         this.bg1 = new Laya.Sprite();
+        this.bg2 = new Laya.Sprite();
         // 加载并显示背景图
         this.bg1.loadImage("war/background.png");
+        this.bg2.loadImage("war/background.png");
         // 把背景图显示在容器内
         this.addChild(this.bg1);
+        this.addChild(this.bg2);
+        //背景2放在背景1的上面
+        this.bg2.pos(0, -852);
+        // 在定时器里面实现背景滚动
+        Laya.timer.frameLoop(1, this, this.onLoop);
+    };
+    BackGround.prototype.onLoop = function () {
+        this.y += 1;
+        if (this.bg1.y + this.y >= 852) {
+            this.bg1.y -= 852 * 2;
+        }
+        if (this.bg2.y + this.y >= 852) {
+            this.bg2.y -= 852 * 2;
+        }
     };
     return BackGround;
 }(Laya.Sprite));
--- src/BackGround.ts
+++ src/BackGround.ts
@@ -15,5 +15,18 @@ class BackGround extends Laya.Sprite{
         // 把背景图显示在容器内
         this.addChild(this.bg1);
         this.addChild(this.bg2);
+        //背景2放在背景1的上面
+        this.bg2.pos(0,-852);
+        // 在定时器里面实现背景滚动
+        Laya.timer.frameLoop(1, this, this.onLoop);
+    }
+    onLoop():void{
+        this.y += 1;
+        if(this.bg1.y + this.y >= 852){
+            this.bg1.y -=852*2;
+        }
+        if(this.bg2.y + this.y >= 852){
+            this.bg2.y -=852*2;
+        }
     }
 }
